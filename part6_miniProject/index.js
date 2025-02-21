e6470b9a537d500ee740e80a0f570c004024cb01
Sachin Chamoli <guptasuraj4455@gmail.com>
2025-02-22 04:23:19 +0530

    public static files setup

--- part6_miniProject/index.js
+++ part6_miniProject/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 app.use(express.json());  //setting up parsers for form, this and next line one line
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'))) //path setup for img,video,text,etc.
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
-    res.send("chal raha hai");
+    res.render("index");
 });
 
 app.listen(3000, ()=>{
