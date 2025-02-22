ba68fb2b85b3bba97b82cb8c1caed129501b2a94
Sachin Chamoli <guptasuraj4455@gmail.com>
2025-02-22 23:32:31 +0530

    express app setup

--- part7/index.js
+++ part7/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express();
+const path = require('path')
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")))
+
 
 app.get('/', function(req, res){
-    res.send("welcome")
+    res.render("index")
 })
 
-app.listen(3000)
+app.listen(3000) 
