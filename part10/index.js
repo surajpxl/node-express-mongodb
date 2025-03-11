afcf68a6a16ebcec98143447287e0afae9e00e49
Sachin Chamoli <guptasuraj4455@gmail.com>
2025-03-11 17:38:35 +0530

    server setup complete

--- part10/index.js
+++ part10/index.js
@@ -23,3 +23,19 @@ app.get('/file/:filename', function(req, res){
         
     })
 })
+
+app.post('/edit', function(req, res){
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err){
+        res.redirect("/")
+    })
+    
+})
+
+app.post('/create', function(req, res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+        res.redirect("/")
+    });
+    
+})
+
+app.listen(3000) 
