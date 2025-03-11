48401c7c4b359fa5ffa6c3ad2a03c652693623f5
Sachin Chamoli <guptasuraj4455@gmail.com>
2025-03-11 17:35:09 +0530

    index  file server making

--- part10/index.js
+++ part10/index.js
@@ -1,2 +1,11 @@
 const express = require('express')
 const app = express()
+
+const path = require('path')
+const fs = require('fs');
+const { log } = require('console');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")))
